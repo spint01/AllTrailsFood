85b09aa961c4af573df5c3147737ab9035cb7e00
Steve Pint <steve@realquantum.com>
2021-11-17 19:46:39 -0600

    Initial Commit

---
+++ AllTrailsFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllTrailsFood
+//
+//  Created by Steven G Pint on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
