cc432d9c2e75db68de3ef1c03ce52d8fa7b4ab30
Steve Pint <steve.pint@gmail.com>
2022-07-25 19:44:08 -0500

    Change - added UIView extension, update PlaceView

---
+++ AllTrailsFood/UI/Common/UIView+extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+extension.swift
+//  AllTrailsFood
+//
+//  Created by Steve Pint on 7/25/22.
+//
+
+import Foundation
+
+public extension UIView {
+    var isHorizontalSizeClassRegular: Bool {
+        return traitCollection.horizontalSizeClass == .regular
+    }
+}
