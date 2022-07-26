774e5814864db11dd7c830d4be428b160bbb38f6
Steve Pint <steve.pint@gmail.com>
2022-07-26 09:06:53 -0500

    Revert "second feature (#2)"
    This reverts commit 4cb42a1a55e76cb6a6b17881ddd0df652ecb4847.

--- AllTrailsFood/UI/Common/UIView+extension.swift
+++ AllTrailsFood/UI/Common/UIView+extension.swift
@@ -5,21 +5,10 @@
 //  Created by Steve Pint on 7/25/22.
 //
 
-import UIKit
+import Foundation
 
-extension UIView {
+public extension UIView {
     var isHorizontalSizeClassRegular: Bool {
         return traitCollection.horizontalSizeClass == .regular
     }
-    
-    // part of second feature
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-
 }
